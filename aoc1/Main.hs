689c8e4abc58bbf600b6bc8119db4d5b7b2da921
Guilherme Torres <guilhermetorres97@gmail.com>
2021-12-02 08:58:21 -0300

    day 1-2

--- aoc1/Main.hs
+++ aoc1/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.IO
 import Control.Monad
+import Data.List (transpose, tails)
 
 readInt :: String -> Int
 readInt = read
@@ -15,7 +16,21 @@ countChangesTail (h:t:ts) x = if t > h then countChangesTail (t:ts) x + 1
 countChanges :: [Int] -> Int
 countChanges s = countChangesTail s 0
 
+sumL :: [Int] -> Int
+sumL = sum
+
+countWindowTail :: [Int] -> Int -> Int
+countWindowTail [] x = x
+countWindowTail s x = do
+    let window = take 3 s
+    let nextWindow = tail $ take 4 s
+    if sumL nextWindow > sumL window then countWindowTail (tail s) (x + 1)
+    else countWindowTail (tail s) x
+
+countWindow :: [Int] -> Int
+countWindow s = countWindowTail s 0
+
 main :: IO ()
 main = do
     contents <- readFile "input"
-    print $ countChanges (map readInt $ lines contents)
+    print $ countWindow (map readInt $ lines contents)
